9234fc1751412805d0a9b5b6c8ac665dfa5f1db9
Rares <rsahleanu@gmail.com>
2020-02-14 18:07:06 +0100

    Initial commit

---
+++ main/Enemy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rasah_s2z0ncs on 14.02.2020.
+//
+
+#include "Enemy.h"
---
+++ main/Enemy.h
@@ -0,0 +1,14 @@
+//
+// Created by rasah_s2z0ncs on 14.02.2020.
+//
+
+#ifndef UNTITLED2_ENEMY_H
+#define UNTITLED2_ENEMY_H
+
+
+class Enemy {
+
+};
+
+
+#endif //UNTITLED2_ENEMY_H
